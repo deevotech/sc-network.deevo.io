2a78763e6cf87d7449f9204b45402a37a56384b9
datlv <datle2011@gmail.com>
2018-08-22 11:24:15 +0700

    fix

--- scripts/install-fabric-ca.sh
+++ scripts/install-fabric-ca.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-wget https://github.com/datlv/fabric-ca/archive/release-1.1.zip --output-document=/tmp/fabric-ca.zip
-unzip /tmp/fabric-ca.zip -d /opt/gopath/src/github.com/hyperledger/ && \
-mv /opt/gopath/src/github.com/hyperledger/fabric-ca-release-1.1 /opt/gopath/src/github.com/hyperledger/fabric-ca && \
+cd /home/ubuntu;
+sudo curl -O https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz;
+sudo tar -xvf go1.9.1.linux-amd64.tar.gz;
+sudo mv go /opt/;
+
+#rm /tmp/hyperledger-fabric-bftsmart.zip;
+rm -rf /opt/gopath/src/github.com/hyperledger/fabric-ca;
+#wget https://github.com/datlv/hyperledger-fabric-bftsmart/archive/release-1.1.zip --output-document=/tmp/hyperledger-fabric-bftsmart.zip
+#unzip /tmp/hyperledger-fabric-bftsmart.zip -d /opt/gopath/src/github.com/hyperledger/ && \
+#mv /opt/gopath/src/github.com/hyperledger/hyperledger-fabric-bftsmart-release-1.1 /opt/gopath/src/github.com/hyperledger/fabric && \
+sudo apt-get install python-pip
+export LC_ALL=C && \
+source ~/.bashrc && \
+source /etc/environment && \
+source ~/.profile;
+cd /opt/gopath/src/github.com/hyperledger && \
+git clone git@github.com:datlv/fabric-ca.git && \
 cd /opt/gopath/src/github.com/hyperledger/fabric-ca && \
-cd cmd/fabric-ca-server
-export GOROOT=/opt/go && GOPATH=/opt/gopath && source ~/.profile && source ~/.bashrc && go build;
-cd ../fabric-ca-client
-export GOROOT=/opt/go && GOPATH=/opt/gopath && source ~/.profile && source ~/.bashrc && go build;
+git checkout release-1.1 && \
+git pull && \
+cd /opt/gopath/src/github.com/hyperledger/fabric-ca && \
+export GOROOT=/opt/go && GOPATH=/opt/gopath && source /etc/environment && cd cmd/fabric-ca-server && go build;
+cd /opt/gopath/src/github.com/hyperledger/fabric-ca && \
+export GOROOT=/opt/go && GOPATH=/opt/gopath && source ~/.profile && source ~/.bashrc && cd cmd/fabric-ca-client && go build;
