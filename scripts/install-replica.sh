7abfd0e01f2c6157a360e9e6c185387a7bdecfa5
datlv <datle2011@gmail.com>
2018-08-22 10:24:16 +0700

    fix

--- scripts/install-replica.sh
+++ scripts/install-replica.sh
@@ -18,8 +18,10 @@ sudo rm -rf /var/lib/apt/lists/* && \
 sudo rm -rf /var/cache/oracle-jdk8-installer;
 	
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+rm /tmp/juds.zip
 wget https://github.com/mcfunley/juds/archive/master.zip --output-document=/tmp/juds.zip;
 
+rm -rf /tmp/juds
 unzip /tmp/juds.zip -d /tmp/juds && \
 cd /tmp/juds/juds-master && \
 ./autoconf.sh && \
@@ -32,8 +34,11 @@ sudo chmod 777 -R /opt/gopath && \
 mkdir -p /opt/gopath/src && \
 mkdir -p /opt/gopath/src/github.com && \
 mkdir -p /opt/gopath/src/github.com/hyperledger;
+rm /tmp/hyperledger-bftsmart.zip
+rm -rf /opt/gopath/src/github.com/hyperledger/hyperledger-bftsmart
 wget https://github.com/datlv/hyperledger-fabric-bftsmart/archive/release-1.1.zip --output-document=/tmp/hyperledger-bftsmart.zip
 unzip /tmp/hyperledger-bftsmart.zip -d /opt/gopath/src/github.com/hyperledger/ && \
+mv /opt/gopath/src/github.com/hyperledger/hyperledger-bftsmart-release-1.1 /opt/gopath/src/github.com/hyperledger/hyperledger-bftsmart && \
 cd /opt/gopath/src/github.com/hyperledger/hyperledger-bftsmart && \
 ant clean && \
 ant;
