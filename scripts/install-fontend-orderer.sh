c319d834da3bd535d085238305d7286bbbf41c34
datlv <datle2011@gmail.com>
2018-08-22 10:42:46 +0700

    fix

--- scripts/install-fontend-orderer.sh
+++ scripts/install-fontend-orderer.sh
@@ -17,28 +17,32 @@ sudo update-ca-certificates -f && \
 sudo rm -rf /var/lib/apt/lists/* && \
 sudo rm -rf /var/cache/oracle-jdk8-installer;
 	
-JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
-export JAVA_HOME
-wget https://github.com/mcfunley/juds/archive/master.zip --output-document=/tmp/juds.zip;
-
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/ && \
+rm /tmp/juds.zip
+wget https://github.com/mcfunley/juds/archive/master.zip --output-document=/tmp/juds.zip && \
+rm -rf /tmp/juds && \
 unzip /tmp/juds.zip -d /tmp/juds && \
 cd /tmp/juds/juds-master && \
 ./autoconf.sh && \
 ./configure && \
 make && \
-make install;
+sudo make install;
 sudo mkdir -p /opt && \
 sudo mkdir -p /opt/gopath/ && \
 sudo chmod 777 -R /opt/gopath && \
 mkdir -p /opt/gopath/src && \
 mkdir -p /opt/gopath/src/github.com && \
 mkdir -p /opt/gopath/src/github.com/hyperledger;
+rm /tmp/hyperledger-bftsmart.zip
+rm -rf /opt/gopath/src/github.com/hyperledger/hyperledger-bftsmart-orderering
 wget https://github.com/datlv/hyperledger-bftsmart-orderering/archive/release-1.1.zip --output-document=/tmp/hyperledger-bftsmart-orderering.zip
 unzip /tmp/hyperledger-bftsmart-orderering.zip -d /opt/gopath/src/github.com/hyperledger/ && \
-cd /opt/gopath/src/github.com/hyperledger/hyperledger-bftsmart-orderering-release-1.1 && \
+mv /opt/gopath/src/github.com/hyperledger/hyperledger-bftsmart-orderering-release-1.1 /opt/gopath/src/github.com/hyperledger/hyperledger-bftsmart-orderering && \
+cd /opt/gopath/src/github.com/hyperledger/hyperledger-bftsmart-orderering && \
 ant clean && \
 ant;
 
+cd /home/ubuntu;
 sudo curl -O https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz;
 sudo tar -xvf go1.9.1.linux-amd64.tar.gz;
 sudo mv go /opt/;
@@ -50,11 +54,9 @@ cd /opt/gopath/src/github.com/hyperledger/fabric && \
 sudo apt-get install python-pip
 export LC_ALL=C
 source ~/.bashrc
-source /etc/environments
+source /etc/environment
 source ~/.profile
 export GOROOT=/opt/go && GOPATH=/opt/gopath && source /etc/environments && sudo ./devenv/setupUbuntuOnPPC64le.sh;
 export GOROOT=/opt/go && GOPATH=/opt/gopath && source ~/.profile && source ~/.bashrc && make dist-clean orderer configtxgen cryptogen;
-
-
 
 
