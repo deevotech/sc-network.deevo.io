5cfbba2534076ed2205500f0c1c16d30fd9a5303
datlv <datle2011@gmail.com>
2018-08-22 10:52:30 +0700

    fix

--- scripts/install-fontend-orderer.sh
+++ scripts/install-fontend-orderer.sh
@@ -48,16 +48,18 @@ sudo tar -xvf go1.9.1.linux-amd64.tar.gz;
 sudo mv go /opt/;
 
 rm /tmp/hyperledger-fabric-bftsmart.zip;
+rm -rf /opt/gopath/src/github.com/hyperledger/fabric;
 wget https://github.com/datlv/hyperledger-fabric-bftsmart/archive/release-1.1.zip --output-document=/tmp/hyperledger-fabric-bftsmart.zip
 unzip /tmp/hyperledger-fabric-bftsmart.zip -d /opt/gopath/src/github.com/hyperledger/ && \
 mv /opt/gopath/src/github.com/hyperledger/hyperledger-fabric-bftsmart-release-1.1 /opt/gopath/src/github.com/hyperledger/fabric && \
-cd /opt/gopath/src/github.com/hyperledger/fabric && \
 sudo apt-get install python-pip
-export LC_ALL=C
-source ~/.bashrc
-source /etc/environment
-source ~/.profile
+export LC_ALL=C && \
+source ~/.bashrc && \
+source /etc/environment && \
+source ~/.profile && \
+cd /opt/gopath/src/github.com/hyperledger/fabric && \
 export GOROOT=/opt/go && GOPATH=/opt/gopath && source /etc/environment && sudo ./devenv/setupUbuntuOnPPC64le.sh;
+cd /opt/gopath/src/github.com/hyperledger/fabric && \
 export GOROOT=/opt/go && GOPATH=/opt/gopath && source ~/.profile && source ~/.bashrc && make dist-clean orderer configtxgen cryptogen;
 
 
