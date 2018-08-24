a7f71765f61c90524730982da8136dd6c31635ff
datlv <datle2011@gmail.com>
2018-08-24 09:14:04 +0700

    fix

--- scripts/register-peer.sh
+++ scripts/register-peer.sh
@@ -47,7 +47,7 @@ function enrollCAAdmin {
    mkdir -p $HOME/cas
    export FABRIC_CA_CLIENT_HOME=$HOME/cas/$CA_NAME
    export FABRIC_CA_CLIENT_TLS_CERTFILES=$CA_CHAINFILE
-   $GOPATH/src/github.com/hyperledger/fabric-ca/cmd/fabric-ca-client/fabric-ca-client enroll -d -u https://rca-${ORG}-admin:rca-${ORG}-adminpw@$CA_HOST:7054
+$GOPATH/src/github.com/hyperledger/fabric-ca/cmd/fabric-ca-client/fabric-ca-client enroll -d -u https://rca-${g}-admin:rca-${g}-adminpw@rca.${g}.deevo.com:7054
 }
 
 function registerIdentities {
