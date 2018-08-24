b331336eda6f84bce31513e277a3d89f36efdeda
datlv <datle2011@gmail.com>
2018-08-24 09:24:16 +0700

    fix

--- scripts/start-root-ca.sh
+++ scripts/start-root-ca.sh
@@ -38,7 +38,8 @@ rm -rf $HOME/fabric-ca/*
 if [ ${r} -eq 1 ] ; then
 	rm -rf ${FABRIC_CA_SERVER_HOME}/*
 	cp -R ${DATA}/rca-${g}-home/* ${FABRIC_CA_SERVER_HOME}/
-else 
+else
+	rm -rf $FABRIC_CA_SERVER_HOME/
 	$GOPATH/src/github.com/hyperledger/fabric-ca/cmd/fabric-ca-server/fabric-ca-server init -b $BOOTSTRAP_USER_PASS
 	# Copy the root CA's signing certificate to the data directory to be used by others
 	mkdir -p ${DATA}
