32de3e4e7339627ed994cc6aff8677a667b104d3
datlv <datle2011@gmail.com>
2018-09-18 15:18:12 +0700

    rm chaincode

--- scripts/remove-docker-chaincode.sh
+++ scripts/remove-docker-chaincode.sh
@@ -4,8 +4,4 @@ if [ "$chaincodeImages" != "" ]; then
   # log "Removing chaincode docker images ..."
    docker rmi -f $chaincodeImages > /dev/null
 fi
-docker kill $(docker ps -q)
-alias docker_clean_images='docker rmi $(docker images -a --filter=dangling=true -q)'
-alias docker_clean_ps='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
-docker rmi $(docker images -a -q)
 
