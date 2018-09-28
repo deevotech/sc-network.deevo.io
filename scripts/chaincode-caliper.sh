9a1fe856ba60ab1fb40492752363c54a6f844b9a
datlv <datle2011@gmail.com>
2018-09-28 14:45:52 +0700

    fix chaincode caliper

--- scripts/chaincode-caliper.sh
+++ scripts/chaincode-caliper.sh
@@ -132,4 +132,97 @@ export ORDERER_CONN_ARGS="$ORDERER_PORT_ARGS --keyfile $CORE_PEER_TLS_CLIENTKEY_
 
 $GOPATH/src/github.com/hyperledger/fabric/build/bin/peer chaincode invoke -C $CHANNEL_NAME -n ${n} -v ${v} -c '{"Args":["open","aaabbbccc","10000"]}' $ORDERER_CONN_ARGS
 
+# test query org5
+ORG=org4
+PEER_HOST=peer0.${ORG}.deevo.com
+PEER_NAME=${PEER_HOST}
+ORG_ADMIN_HOME=$DATA/orgs/$ORG/admin
+CA_CHAINFILE=${DATA}/${ORG}-ca-cert.pem
+export FABRIC_CA_CLIENT_HOME=$ORG_ADMIN_HOME
+export FABRIC_CA_CLIENT_TLS_CERTFILES=$CA_CHAINFILE
+export CORE_PEER_MSPCONFIGPATH=$ORG_ADMIN_HOME/msp
+export CORE_PEER_ID=$PEER_HOST
+export CORE_PEER_ADDRESS=$PEER_HOST:7051
+export CORE_PEER_LOCALMSPID=${ORG}MSP
+export CORE_LOGGING_LEVEL=DEBUG
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_TLS_CLIENTAUTHREQUIRED=true
+export CORE_PEER_TLS_ROOTCERT_FILE=$CA_CHAINFILE
+export CORE_PEER_TLS_CLIENTCERT_FILE=$DATA/tls/$PEER_NAME-cli-client.crt
+export CORE_PEER_TLS_CLIENTKEY_FILE=$DATA/tls/$PEER_NAME-cli-client.key
+export CORE_PEER_PROFILE_ENABLED=true
+# gossip variables
+export CORE_PEER_GOSSIP_USELEADERELECTION=true
+export CORE_PEER_GOSSIP_ORGLEADER=false
+export ORDERER_PORT_ARGS=" -o orderer0.org0.deevo.com:7050 --tls --cafile $DATA/org0-ca-cert.pem --clientauth"
+export ORDERER_CONN_ARGS="$ORDERER_PORT_ARGS --keyfile $CORE_PEER_TLS_CLIENTKEY_FILE --certfile $CORE_PEER_TLS_CLIENTCERT_FILE"
+echo $ORDERER_CONN_ARGS
+
+echo "Instantiating chaincode on $PEER_HOST ..."
+export ORDERER_PORT_ARGS=" -o orderer0.org0.deevo.com:7050 --tls --cafile $DATA/org0-ca-cert.pem --clientauth"
+export ORDERER_CONN_ARGS="$ORDERER_PORT_ARGS --keyfile $CORE_PEER_TLS_CLIENTKEY_FILE --certfile $CORE_PEER_TLS_CLIENTCERT_FILE"
+
+$GOPATH/src/github.com/hyperledger/fabric/build/bin/peer chaincode invoke -C $CHANNEL_NAME -n ${n} -v ${v} -c '{"Args":["open","aaabbbddd","10000"]}' $ORDERER_CONN_ARGS
+# test query org5
+ORG=org3
+PEER_HOST=peer0.${ORG}.deevo.com
+PEER_NAME=${PEER_HOST}
+ORG_ADMIN_HOME=$DATA/orgs/$ORG/admin
+CA_CHAINFILE=${DATA}/${ORG}-ca-cert.pem
+export FABRIC_CA_CLIENT_HOME=$ORG_ADMIN_HOME
+export FABRIC_CA_CLIENT_TLS_CERTFILES=$CA_CHAINFILE
+export CORE_PEER_MSPCONFIGPATH=$ORG_ADMIN_HOME/msp
+export CORE_PEER_ID=$PEER_HOST
+export CORE_PEER_ADDRESS=$PEER_HOST:7051
+export CORE_PEER_LOCALMSPID=${ORG}MSP
+export CORE_LOGGING_LEVEL=DEBUG
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_TLS_CLIENTAUTHREQUIRED=true
+export CORE_PEER_TLS_ROOTCERT_FILE=$CA_CHAINFILE
+export CORE_PEER_TLS_CLIENTCERT_FILE=$DATA/tls/$PEER_NAME-cli-client.crt
+export CORE_PEER_TLS_CLIENTKEY_FILE=$DATA/tls/$PEER_NAME-cli-client.key
+export CORE_PEER_PROFILE_ENABLED=true
+# gossip variables
+export CORE_PEER_GOSSIP_USELEADERELECTION=true
+export CORE_PEER_GOSSIP_ORGLEADER=false
+export ORDERER_PORT_ARGS=" -o orderer0.org0.deevo.com:7050 --tls --cafile $DATA/org0-ca-cert.pem --clientauth"
+export ORDERER_CONN_ARGS="$ORDERER_PORT_ARGS --keyfile $CORE_PEER_TLS_CLIENTKEY_FILE --certfile $CORE_PEER_TLS_CLIENTCERT_FILE"
+echo $ORDERER_CONN_ARGS
+
+echo "Instantiating chaincode on $PEER_HOST ..."
+export ORDERER_PORT_ARGS=" -o orderer0.org0.deevo.com:7050 --tls --cafile $DATA/org0-ca-cert.pem --clientauth"
+export ORDERER_CONN_ARGS="$ORDERER_PORT_ARGS --keyfile $CORE_PEER_TLS_CLIENTKEY_FILE --certfile $CORE_PEER_TLS_CLIENTCERT_FILE"
+
+$GOPATH/src/github.com/hyperledger/fabric/build/bin/peer chaincode invoke -C $CHANNEL_NAME -n ${n} -v ${v} -c '{"Args":["open","aaabbbeee","10000"]}' $ORDERER_CONN_ARGS
+# test query org5
+ORG=org2
+PEER_HOST=peer0.${ORG}.deevo.com
+PEER_NAME=${PEER_HOST}
+ORG_ADMIN_HOME=$DATA/orgs/$ORG/admin
+CA_CHAINFILE=${DATA}/${ORG}-ca-cert.pem
+export FABRIC_CA_CLIENT_HOME=$ORG_ADMIN_HOME
+export FABRIC_CA_CLIENT_TLS_CERTFILES=$CA_CHAINFILE
+export CORE_PEER_MSPCONFIGPATH=$ORG_ADMIN_HOME/msp
+export CORE_PEER_ID=$PEER_HOST
+export CORE_PEER_ADDRESS=$PEER_HOST:7051
+export CORE_PEER_LOCALMSPID=${ORG}MSP
+export CORE_LOGGING_LEVEL=DEBUG
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_TLS_CLIENTAUTHREQUIRED=true
+export CORE_PEER_TLS_ROOTCERT_FILE=$CA_CHAINFILE
+export CORE_PEER_TLS_CLIENTCERT_FILE=$DATA/tls/$PEER_NAME-cli-client.crt
+export CORE_PEER_TLS_CLIENTKEY_FILE=$DATA/tls/$PEER_NAME-cli-client.key
+export CORE_PEER_PROFILE_ENABLED=true
+# gossip variables
+export CORE_PEER_GOSSIP_USELEADERELECTION=true
+export CORE_PEER_GOSSIP_ORGLEADER=false
+export ORDERER_PORT_ARGS=" -o orderer0.org0.deevo.com:7050 --tls --cafile $DATA/org0-ca-cert.pem --clientauth"
+export ORDERER_CONN_ARGS="$ORDERER_PORT_ARGS --keyfile $CORE_PEER_TLS_CLIENTKEY_FILE --certfile $CORE_PEER_TLS_CLIENTCERT_FILE"
+echo $ORDERER_CONN_ARGS
+
+echo "Instantiating chaincode on $PEER_HOST ..."
+export ORDERER_PORT_ARGS=" -o orderer0.org0.deevo.com:7050 --tls --cafile $DATA/org0-ca-cert.pem --clientauth"
+export ORDERER_CONN_ARGS="$ORDERER_PORT_ARGS --keyfile $CORE_PEER_TLS_CLIENTKEY_FILE --certfile $CORE_PEER_TLS_CLIENTCERT_FILE"
+
+$GOPATH/src/github.com/hyperledger/fabric/build/bin/peer chaincode invoke -C $CHANNEL_NAME -n ${n} -v ${v} -c '{"Args":["open","aaabbbfff","10000"]}' $ORDERER_CONN_ARGS
 echo "done";
