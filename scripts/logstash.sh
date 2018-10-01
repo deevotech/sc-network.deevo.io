b296637c8c50afae63c2427edcf48edbf0cc9686
datlv <datle2011@gmail.com>
2018-10-01 12:01:27 +0700

    add file log pid

---
+++ scripts/logstash.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+usage() { echo "Usage: $0 [-n <name>]" 1>&2; exit 1; }
+while getopts ":n:" o; do
+    case "${o}" in
+        n)
+            n=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+if [ -z "${n}" ] ; then
+    usage
+fi
+logpid() { while sleep 1; do  ps -p $1 -o pcpu= -o pmem= ; done; }
+pid=$(pidof ${n})
+logpid $pid > ./data/logs/pid-${n}.log 2>&1 &
