c53644af056a688c8375eded4090e9a8cd1cd273
datlv <datle2011@gmail.com>
2018-10-05 16:47:49 +0700

    init mvp

--- scripts/chaincode-mvp-query-auditor.sh
+++ scripts/chaincode-mvp-query-auditor.sh
@@ -69,4 +69,4 @@ export ORDERER_PORT_ARGS=" -o orderer0.org0.deevo.com:7050 --tls --cafile $DATA/
 export ORDERER_CONN_ARGS="$ORDERER_PORT_ARGS --keyfile $CORE_PEER_TLS_CLIENTKEY_FILE --certfile $CORE_PEER_TLS_CLIENTCERT_FILE"
 echo $ORDERER_CONN_ARGS
 
-$GOPATH/src/github.com/hyperledger/fabric/build/bin/peer chaincode query -C $CHANNEL_NAME -n ${n} -v ${v}  -c '{"Args":["getObject","'${i}'"}' $ORDERER_CONN_ARGS
+$GOPATH/src/github.com/hyperledger/fabric/build/bin/peer chaincode query -C $CHANNEL_NAME -n ${n} -v ${v}  -c '{"Args":["getObject","'${i}'"]}' $ORDERER_CONN_ARGS
