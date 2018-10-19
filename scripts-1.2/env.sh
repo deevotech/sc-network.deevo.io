c7db8b5094eaad7ea246a8e63582441f4e9e605f
datlv <datle2011@gmail.com>
2018-10-19 12:18:57 +0700

    init 1.2

--- scripts-1.2/env.sh
+++ scripts-1.2/env.sh
@@ -402,3 +402,35 @@ function fatal {
    log "FATAL: $*"
    exit 1
 }
+
+function genMSPCerts() {
+	if [ $# -ne 6 ]; then
+		echo "Usage: genMSPCerts <host name> <name> <password> <org> <ca host> <msp dir>: $*"
+		exit 1
+	fi
+
+	HOST_NAME=$1
+	NAME=$2
+	PASSWORD=$3
+	ORG=$4
+	CA_HOST_NAME=$5
+	MSP_DIR=$6
+
+	logr "Enroll to get peer's TLS cert"
+
+	mkdir -p $MSP_DIR
+
+	fabric-ca-client enroll -d --enrollment.profile tls -u https://$NAME:$PASSWORD@$CA_HOST_NAME:7054 -M $MSP_DIR --csr.hosts $HOST_NAME --csr.names C=US,ST="California",O=${ORG},OU=COP
+
+	# Copy CA certs
+	mkdir $MSP_DIR/tlscacerts
+	mkdir $MSP_DIR/cacerts
+    cp $ORG_MSP/cacerts/* $MSP_DIR/tlscacerts
+	cp $ORG_MSP/cacerts/* $MSP_DIR/cacerts
+}
+
+function logr() {
+	log $*
+	log $* >>$RUN_SUMPATH
+}
+
--- scripts-1.2/start-root-ca.sh
+++ scripts-1.2/start-root-ca.sh
@@ -4,6 +4,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+SDIR=$(dirname "$0")
+source $SDIR/env.sh
 usage() { echo "Usage: $0 [-g <orgname>] [-r <restart>]" 1>&2; exit 1; }
 while getopts ":g:r:" o; do
     case "${o}" in
@@ -32,7 +34,12 @@ export FABRIC_CA_SERVER_DEBUG=true
 export BOOTSTRAP_USER_PASS=rca-${g}-admin:rca-${g}-adminpw
 export TARGET_CERTFILE=$DATA/${g}-ca-cert.pem
 export FABRIC_CA_SERVER_CA_NAME=rca.${g}.deevo.com
-export FABRIC_ORGS="org0 org1 org2 org3 org4 org5"
+export FABRIC_ORGS="ordering-nodes org0 org1 org2 org3 org4 org5"
+#export FABRIC_CA_SERVER_CA_NAME=rca.ordering.bft
+#export FABRIC_CA_SERVER_CSR_CN=rca.ordering.bft
+#export FABRIC_CA_SERVER_CSR_HOSTS=rca.ordering.bft
+export FABRIC_CA_SERVER_TLS_CERTFILE=/etc/hyperledger/fabric-ca-server-config/rca.ordering.bft-cert.pem
+export FABRIC_CA_SERVER_TLS_KEYFILE=/etc/hyperledger/fabric-ca-server-config/rca.ordering.bft-cert.key
 rm -rf $HOME/fabric-ca/*
 # Initialize the root CA
 if [ ${r} -eq 1 ] ; then
@@ -40,7 +47,7 @@ if [ ${r} -eq 1 ] ; then
 	cp -R ${DATA}/rca-${g}-home/* ${FABRIC_CA_SERVER_HOME}/
 else
 	rm -rf $FABRIC_CA_SERVER_HOME/
-	$GOPATH/src/github.com/hyperledger/fabric-ca/cmd/fabric-ca-server/fabric-ca-server init -b $BOOTSTRAP_USER_PASS
+	fabric-ca-server init -b $BOOTSTRAP_USER_PASS
 	# Copy the root CA's signing certificate to the data directory to be used by others
 	mkdir -p ${DATA}
 	rm -rf ${DATA}/*
@@ -60,4 +67,26 @@ fi
 # Start the root CA
 mkdir -p data
 mkdir -p data/logs
-$GOPATH/src/github.com/hyperledger/fabric-ca/cmd/fabric-ca-server/fabric-ca-server start > ./data/logs/rca.${g}.deevo.com 2>&1 &
+export RUN_SUMPATH=./data/logs/ca-${ORG}.log
+
+# Add the custom orgs
+for o in $FABRIC_ORGS; do
+	aff=$aff"\n   $o: []"
+done
+logr $aff
+perl -0777 -i.original -pe "s/affiliations:\n   org1:\n      - department1\n      - department2\n   org2:\n      - department1/affiliations:$aff/" $FABRIC_CA_SERVER_HOME/fabric-ca-server-config.yaml
+sed -i "s/ST: \"North Carolina\"/ST: \"California\"/g" \
+	$FABRIC_CA_SERVER_HOME/fabric-ca-server-config.yaml
+sed -i "s/OU: Fabric/OU: COP/g" \
+	$FABRIC_CA_SERVER_HOME/fabric-ca-server-config.yaml
+sed -i "s/O: Hyperledger/O: $ORG/g" \
+	$FABRIC_CA_SERVER_HOME/fabric-ca-server-config.yaml
+mkdir -p /etc/hyperledger/fabric-ca-server-config
+cp $FABRIC_CA_SERVER_HOME/fabric-ca-server-config.yaml /etc/hyperledger/fabric-ca-server-config/fabric-ca-server-config.yaml
+
+fabric-ca-server init -b $BOOTSTRAP_USER_PASS
+cp $FABRIC_CA_SERVER_HOME/ca-cert.pem $FABRIC_CA_SERVER_TLS_KEYFILE
+
+logr "Start CA server"
+
+fabric-ca-server start --ca.certfile $FABRIC_CA_SERVER_TLS_CERTFILE --ca.keyfile $FABRIC_CA_SERVER_TLS_KEYFILE -b $BOOTSTRAP_USER_PASS -d 2>&1 | tee -a  $RUN_SUMPATH
