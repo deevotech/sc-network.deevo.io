274418096b6aaa237e26c6c6676e7ab34617e715
Quang Phan <pquocquang1102@gmail.com>
2018-10-30 13:01:59 +0700

    #1 update install replica

--- scripts-1.2/install-frontend-orderer.sh
+++ scripts-1.2/install-frontend-orderer.sh
@@ -1,67 +1,58 @@
 #!/bin/bash
 
-sudo apt-get clean && \
-rm -rf /var/lib/apt/lists/* && \
-rm -rf /var/cache/oracle-jdk8-installer;
+sudo apt-get clean
+sudo rm -rf /var/cache/oracle-jdk8-installer
 
-sudo apt-get update -y && \
-apt-get install -y default-jre && \
-apt-get install -y default-jdk && \
-rm -rf /var/lib/apt/lists/* && \
-rm -rf /var/cache/oracle-jdk8-installer;
+sudo apt-get update -y
+sudo apt-get install -y default-jre
+sudo apt-get install -y default-jdk
+sudo rm -rf /var/cache/oracle-jdk8-installer
 
 sudo update-alternatives --config javac
 
-sudo apt-get update && \
-apt-get install -y ant && \
-apt-get install -y unzip && \
-apt-get install -y wget && \
-apt-get install -y autoconf && \
-apt-get install -y build-essential && \
-apt-get install -y libc6-dev-i386 && \
-apt-get clean;
+sudo apt-get update
+sudo apt-get install -y ant unzip wget autoconf build-essential libc6-dev-i386
+sudo apt-get clean
 
-sudo apt-get update \
-        && apt-get install -y apt-utils python-dev \
-        && apt-get install -y libsnappy-dev zlib1g-dev libbz2-dev libyaml-dev libltdl-dev libtool libc6 \
-        && apt-get install -y python-pip \
-        && apt-get install -y tree jq unzip\
-        && rm -rf /var/cache/apt;
+sudo apt-get install -y apt-utils python-dev
+sudo apt-get install -y libsnappy-dev zlib1g-dev libbz2-dev libyaml-dev libltdl-dev libtool libc6
+sudo apt-get install -y python-pip
+sudo apt-get install -y tree jq unzip
+sudo rm -rf /var/cache/apt
 
-mkdir -p $GOPATH/src/github.com/hyperledger
-cd $GOPATH/src/github.com/hyperledger
+rm -rf ~/juds
+mkdir ~/juds
+cd ~/juds
 
-wget https://github.com/mcfunley/juds/archive/master.zip --output-document=/tmp/juds.zip;
+git clone https://github.com/mcfunley/juds
 
 echo $(ls $JAVA_HOME/bin)
 
-unzip /tmp/juds.zip -d /tmp/juds;
-cd /tmp/juds/juds-master;
-cd /tmp/juds/juds-master && \
-./autoconf.sh && \
-./configure && \
-make && \
-make install;
-rm -rf /tmp/juds.zip
-rm -rf /tmp/juds
+cd juds
 
+./autoconf.sh
+./configure
+make
+make install
+
+mkdir -p $GOPATH/src/github.com/hyperledger
 cd $GOPATH/src/github.com/hyperledger
-git clone https://github.com/deevotech/fabric-orderingservice -b release-1.2-deevo 
-cd $GOPATH/src/github.com/hyperledger/fabric-orderingservice 
-ant clean 
-ant;
+git clone https://github.com/deevotech/fabric-orderingservice -b release-1.2-deevo
+cd $GOPATH/src/github.com/hyperledger/fabric-orderingservice
+ant clean
+ant
 
-go get github.com/golang/protobuf/protoc-gen-go \
-        && go get github.com/kardianos/govendor \
-        && go get golang.org/x/lint/golint \
-        && go get golang.org/x/tools/cmd/goimports \
-        && go get github.com/onsi/ginkgo/ginkgo \
-        && go get github.com/axw/gocov/... \
-        && go get github.com/client9/misspell/cmd/misspell \
-        && go get github.com/AlekSi/gocov-xml
+go get github.com/golang/protobuf/protoc-gen-go
+go get github.com/kardianos/govendor
+go get golang.org/x/lint/golint
+go get golang.org/x/tools/cmd/goimports
+go get github.com/onsi/ginkgo/ginkgo
+go get github.com/axw/gocov/...
+go get github.com/client9/misspell/cmd/misspell
+go get github.com/AlekSi/gocov-xml
 
 # Clone the Hyperledger Fabric code and cp sample config files
-cd $GOPATH/src/github.com/hyperledger 
+cd $GOPATH/src/github.com/hyperledger
 git clone https://github.com/deevotech/fabric -brelease-1.2-deevo
 
 FABRIC_ROOT=$GOPATH/src/github.com/hyperledger/fabric
--- scripts-1.2/install-replica.sh
+++ scripts-1.2/install-replica.sh
@@ -1,43 +1,35 @@
 #!/bin/bash
 
-sudo apt-get clean &&
-	rm -rf /var/lib/apt/lists/* &&
-	rm -rf /var/cache/oracle-jdk8-installer
+sudo apt-get clean
+sudo rm -rf /var/cache/oracle-jdk8-installer
 
-sudo apt-get update -y &&
-	apt-get install -y default-jre &&
-	apt-get install -y default-jdk &&
-	rm -rf /var/lib/apt/lists/* &&
-	rm -rf /var/cache/oracle-jdk8-installer
+sudo apt-get update -y
+sudo apt-get install -y default-jre
+sudo apt-get install -y default-jdk
+sudo rm -rf /var/cache/oracle-jdk8-installer
 
 sudo update-alternatives --config javac
 
-sudo apt-get update &&
-	apt-get install -y ant &&
-	apt-get install -y unzip &&
-	apt-get install -y wget &&
-	apt-get install -y autoconf &&
-	apt-get install -y build-essential &&
-	apt-get install -y libc6-dev-i386 &&
-	apt-get clean
+sudo apt-get update
+sudo apt-get install -y ant unzip wget autoconf build-essential libc6-dev-i386
+sudo apt-get clean
 
-mkdir -p $GOPATH/src/github.com/hyperledger
-cd $GOPATH/src/github.com/hyperledger
+rm -rf ~/juds
+mkdir ~/juds
+cd ~/juds
 
-wget https://github.com/mcfunley/juds/archive/master.zip --output-document=/tmp/juds.zip
+git clone https://github.com/mcfunley/juds
 
 echo $(ls $JAVA_HOME/bin)
 
-unzip /tmp/juds.zip -d /tmp/juds
-cd /tmp/juds/juds-master
-cd /tmp/juds/juds-master &&
-	./autoconf.sh &&
-	./configure &&
-	make &&
-	make install
-rm -rf /tmp/juds.zip
-rm -rf /tmp/juds
+cd juds
 
+./autoconf.sh
+./configure
+make
+make install
+
+mkdir -p $GOPATH/src/github.com/hyperledger
 cd $GOPATH/src/github.com/hyperledger
 git clone https://github.com/deevotech/fabric-orderingservice -b release-1.2-deevo
 cd $GOPATH/src/github.com/hyperledger/fabric-orderingservice
